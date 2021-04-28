56d72e3fa973a38ece293f094133178835c40e87
Narayana Imestin <imestin@tuta.io>
2021-04-28 15:01:38 +0200

    Removes markdown parsers that are not used

--- article.js
+++ article.js
@@ -8,12 +8,11 @@ async function contentLoader() {
     let mdText = await fetch(gateway + 'ipfs/' + cid + '/article.md').then(response => response.text())
     let article = document.getElementById("articleBox");                                // This will be the main content
     document.getElementById("loading").style = "display: none";                         // Hide loading screen
-//    article.innerHTML = parseMarkdown(mdText, cid);
-		//article.innerHTML = markdown.toHTML(mdText)
-		marked.setOptions({
-			baseUrl: 'https://ipfs.io/' + 'ipfs/' + cid + '/'
-		})
-		article.innerHTML = marked(mdText);
+
+	marked.setOptions({																	// Sets the article folder as base url
+		baseUrl: 'https://ipfs.io/' + 'ipfs/' + cid + '/'
+	})
+	article.innerHTML = marked(mdText);													// Parse markdown 
 
     let metaObj = await fetchMeta(cid);                                                 // We load this secondly, because fetch might take a lot of time and this is just meta
     document.title = metaObj.title;
